6dcb9a80073f7166e0827a0434ce014755d63ec3
Adam Chao <adam@thinkaliker.com>
2015-06-06 22:54:13 -0700

    fixed everything

---
+++ HexClock/HexClock.Windows/js/scaled.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    "use strict";
+    WinJS.UI.Pages.define("/pages/scaled.html", {
+
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+        },
+
+        unload: function () {
+            // TODO: Respond to navigations away from this page.
+        },
+
+        updateLayout: function (element) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in layout.
+        }
+    });
+})();
---
+++ HexClock/HexClock.Windows/js/script.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    "use strict";
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    var nav = WinJS.Navigation;
+
+    app.addEventListener("activated", function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+
+            args.setPromise(WinJS.UI.processAll().then(function () {
+                if (nav.location) {
+                    nav.history.current.initialPlaceholder = true;
+                    return nav.navigate(nav.location, nav.state);
+                } else {
+                    return nav.navigate(Application.navigator.home);
+                }
+            }));
+        }
+    });
+
+
+    app.start();
+})();
